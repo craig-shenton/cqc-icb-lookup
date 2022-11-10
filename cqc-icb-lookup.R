2415e278a01b08befd10ad28dc9d20d0d878830f
craig-shenton <craig.shenton@nhs.net>
2022-11-10 09:53:21 +0000

    data in csv

--- cqc-icb-lookup.R
+++ cqc-icb-lookup.R
@@ -22,6 +22,7 @@ library(tidyverse)
 library(tidyr)
 library(dplyr)
 library(readxl)
+library(RCurl)
 
 # Download CQC Codes dataset
 # -------------------------------------------------------------------------
